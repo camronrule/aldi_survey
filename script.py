83ad4bf0d5a1acb64558f21c73ebc1b57f5b6d4c
cmrule <cmrule@wm.edu>
2023-09-01 16:35:20 -0400

    Change find by id to css

--- script.py
+++ script.py
@@ -12,12 +12,14 @@ import random
 print('Please enter the code located on the bottom of the receipt')
 print('Enter the code separated by hyphens, with no other characters')
 print('e.g., 0123-456-789-012-345')
-receipt_code = input().split('-')
+#receipt_code = input().split('-')
+receipt_code = ["2685","480","002","005","016"]
 
 #take in survey date
 print('Please enter the date on the receipt in DD-MM-YYYY format, separated by hyphens')
 print('e.g., 06-20-2023')
-receipt_date = input().split('-')
+#receipt_date = input().split('-')
+receipt_date = ["09","01","23"]
 
 #take in survey time
 print("Please enter the time the survey was printed, by typing 0, 1, 2, or 3")
@@ -25,7 +27,8 @@ print("0: 8AM-12PM")
 print("1: 12PM-3PM")
 print("2: 3PM-5PM")
 print("3: 5PM-9PM")
-receipt_time = input()
+#receipt_time = input()
+receipt_time = "0"
 
 #user info is stored already
 #change this if others are using the program
@@ -87,117 +90,107 @@ while counter <= NUM_CHECKBOXES_TO_SELECT:
     print("clicking checkbox index:", checkbox_index)
     browser.find_by_css('label.ui-checkbox')[r.randint(0, (len(checkboxes)-1))].check()
     counter+=1
-    time.sleep(1) 
 
-element_rate = browser.find_by_id('nextPageLink').first.click() #next
+browser.find_by_id('nextPageLink').first.click() #next
 
 #rate satisfaction with location
 browser.find_by_css('div.last')[1].click()
-#element_rate = browser.find_by_id("prompt_484675")
-#element_rate.select("1114559")
-
-time.sleep(5)
 
 #rate recommending to a friend
 browser.find_by_css('div.last')[2].click()
-#element_rate.select("1114564")
-
-time.sleep(5)
-
-
 browser.find_by_id('nextPageLink').first.click() #next
+
+#################################
+time.sleep(1.5)
 #skip page describing experience
 browser.find_by_id('nextPageLink').first.click() #next
 
+#################################
+time.sleep(1.5)
 #rate cleanliness of store
-element_rate = browser.find_by_id("prompt_484678")
-element_rate.select("1114574")
+browser.find_by_css('div.last')[1].click()
 
 #rate neatness of store
-element_rate = browser.find_by_id("484679")
-element_rate.select("1114579")
+browser.find_by_css('div.last')[2].click()
 
 #rate ease of moving in the store
-element_rate = browser.find_by_id("484680")
-element_rate.select("1114584")
-
+browser.find_by_css('div.last')[3].click()
 browser.find_by_id('nextPageLink').first.click() #next
 
+#################################
+time.sleep(1.5)
 #rate friendliness of staff
-element_rate = browser.find_by_id("485082")
-element_rate.select("1115609")
+browser.find_by_css("div.last")[1].click()
 
 #rate happiness with quality of products
-element_rate = browser.find_by_id("484681")
-element_rate.select("1114589")
-
-#rate time at checkout
-element_rate = browser.find_by_id("484682")
-element_rate.select("1114594")
-
-browser.find_by_id('nextPageLink').first.click() #next
+browser.find_by_css('div.last')[2].click()
 
 #were products in stock
-element_yn = browser.find_by_id("prompt_485081")
-element_yn.select("1115603")
+browser.find_by_text("Yes").click()
+browser.find_by_id('nextPageLink').first.click() #next
 
+#################################
+time.sleep(1.5)
 #did the cashier handle product well
-element_yn = browser.find_by_id("prompt_485083")
-element_yn.select("1114595")
+browser.find_by_text("Yes").click()
+browser.find_by_id('nextPageLink').first.click() #next
+
+#################################
+time.sleep(1.5)
+#time spent at checkout met my expectations
+browser.find_by_css('div.last').click()
 
 #did the cashier ring things up correctly
-element_yn = browser.find_by_id("prompt_386747")
-element_yn.select("858735")
-
+browser.find_by_text("Yes").click()
 browser.find_by_id('nextPageLink').first.click() #next
 
+##################################
+time.sleep(1.5)
 #were cleaning wipes available
-element_yn = browser.find_by_id("prompt_449830")
-element_yn.select("1036834")
+browser.find_by_text("1").click()
 
 #was hand sanitizer available
-element_yn = browser.find_by_id("prompt_461204")
-element_yn.select("1061008")
-
+browser.find_by_text("1").last.click()
 browser.find_by_id('nextPageLink').first.click() #next
 
+###################################
+time.sleep(1.5)
 #what is ur experience with aldi app
-element_mult = browser.find_by_id("prompt_485291")
-element_mult.select("1115949")
-
+browser.find_by_text("I know ALDI has a mobile app but I have never used it").click()
 browser.find_by_id('nextPageLink').first.click() #next
 
+###################################
+time.sleep(1.5)
 #what is ur experience with aldi grocery delivery
-element_mult = browser.find_by_id("prompt_485301")
-element_mult.select("1115981")
+browser.find_by_text("I know ALDI has grocery delivery but I have never tried it").click()
 
 #what is ur familiarity with aldi curbside pickup
-element_mult = browser.find_by_id("prompt_485303")
-element_mult.select("1115989")
-
+browser.find_by_text("I know ALDI has curbside pickup but I have never tried it").click()
 browser.find_by_id('nextPageLink').first.click() #next
 
+###################################
+time.sleep(1.5)
 #describe what products you want (optional)
 browser.find_by_id('nextPageLink').first.click() #next
 
+####################################
+time.sleep(1.5)
 #demographic page
 #age (18-24)
-element_mult = browser.find_by_id("prompt_386076")
-element_mult.select("857079")
+browser.find_by_text("18 to 24").click()
 
 #how often do you shop at aldi (1+ /week)
-element_mult = browser.find_by_id("prompt_485306")
-element_mult.select("1115997")
+browser.find_by_text("One or more times per week").click()
 
 #do you have any children (no)
-element_mult = browser.find_by_id("prompt_485308")
-element_mult.select("1116006")
+browser.find_by_text("No").click()
 
 #household income (25-50k)
-element_mult = browser.find_by_id("prompt_485309")
-element_mult.select("1116008")
-
+browser.find_by_text("49").click()
 browser.find_by_id('nextPageLink').first.click() #next
+
+####################################
+time.sleep(1.5)
 
 #would you like to enter for gift card
 element_mult = browser.find_by_id("prompt_403404")
